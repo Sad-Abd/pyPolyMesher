4d478d598928a64535cb63aa5fa651995c48be04
Sad-Abd <abedisadjad@gmail.com>
2023-10-17 14:38:52 +0330

    Added another test with arbitrary points

--- test1.py
+++ test1.py
@@ -2,20 +2,30 @@ import numpy as np
 from pyMbbDomain import MbbDomain
 from pyPolyMesher import PolyMesher
 
-nelx = 5
-nely = 4
-
-dx = 3 / nelx
-dy = 1 / nely
-
-x_range = np.arange(dx / 2, 3, dx)
-y_range = np.arange(dy / 2, 1, dy)
-
-X, Y = np.meshgrid(x_range, y_range)
-P = np.column_stack((X.ravel(), Y.ravel()))
 
 # for structured mesh 
-PolyMesher(MbbDomain, 20, 30, P)
+# nelx = 5
+# nely = 4
+# dx = 3 / nelx
+# dy = 1 / nely
+# x_range = np.arange(dx / 2, 3, dx)
+# y_range = np.arange(dy / 2, 1, dy)
+# X, Y = np.meshgrid(x_range, y_range)
+# P = np.column_stack((X.ravel(), Y.ravel()))
+# PolyMesher(MbbDomain, 20, 30, P)
+
+# for unstructured mesh with given points (can be used to compare results with MATLAB code)
+# P = np.array([[2.60026522, 0.03321281],
+#  [0.01350291, 0.39790721],
+#  [1.06513347, 0.40730687],
+#  [2.54716707, 0.76359476],
+#  [1.29260857, 0.39074023],
+#  [2.41308243, 0.38506563],
+#  [2.22468734, 0.69673122],
+#  [0.95129956, 0.09203691],
+#  [0.6433727,  0.67682601],
+#  [2.1696533,  0.3882989 ]])
+# PolyMesher(MbbDomain, 10, 50, P)
 
 # for unstructured mesh
-# PolyMesher(MbbDomain, 10, 30)
+PolyMesher(MbbDomain, 50, 100)
