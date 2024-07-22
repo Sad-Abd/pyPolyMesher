971b33bbb9671e6e94dee3634e968bb5e87ac6e4
Sadjad <65682889+Sad-Abd@users.noreply.github.com>
2024-07-22 15:55:15 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(
     name="PolyMesher",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     author="Sad-Abd",
